92dbff00bbbbfd6dd12ab38d58fdfec5da039ee8
misterjason95 <u20181g835@upc.edu.pe>
2021-04-26 12:52:28 -0500

    Update ActivityCursos.java

--- app/src/main/java/com/upc/appcentroidiomas/ActivityCursos.java
+++ app/src/main/java/com/upc/appcentroidiomas/ActivityCursos.java
@@ -1,8 +1,8 @@
 package com.upc.appcentroidiomas;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.os.Bundle;
+
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
