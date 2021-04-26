52a6881e9b801b6a02a6255c329ccd4b594e09fc
misterjason95 <u20181g835@upc.edu.pe>
2021-04-26 12:46:49 -0500

    Update ActivityCursos.java

--- app/src/main/java/com/upc/appcentroidiomas/ActivityCursos.java
+++ app/src/main/java/com/upc/appcentroidiomas/ActivityCursos.java
@@ -3,11 +3,7 @@ package com.upc.appcentroidiomas;
 
 
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
