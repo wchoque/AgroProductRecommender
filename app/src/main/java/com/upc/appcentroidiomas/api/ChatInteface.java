17bd49b44b08c76f2a03c9308181e8f109f2ea06
U202020534 <U202020534@upc.edu.pe>
2021-05-03 18:23:42 -0500

    Add chat history api method

--- app/src/main/java/com/upc/appcentroidiomas/api/ChatInteface.java
+++ app/src/main/java/com/upc/appcentroidiomas/api/ChatInteface.java
@@ -1,13 +1,19 @@
 package com.upc.appcentroidiomas.api;
 
+import com.upc.appcentroidiomas.data.model.AvailableChatUserResponse;
 import com.upc.appcentroidiomas.data.model.NewMessageModel;
 import com.upc.appcentroidiomas.data.model.NewMessageResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ChatInteface {
     @POST("ChatMessage")
     Call<NewMessageResponse> SendNewMessage(@Body NewMessageModel loginModel);
+
+    @GET("ChatMessage/GetHistoryChat/{userIdFrom}/{userIdTo}")
+    Call<AvailableChatUserResponse> get(@Path("userIdFrom") int userIdFrom, @Path("userIdTo") int userIdTo);
 }
