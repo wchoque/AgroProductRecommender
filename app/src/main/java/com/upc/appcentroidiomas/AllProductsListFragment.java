3984c77b4c6f221aaa57e22fa52f674274cf29dd
Wilber Choque <w.choque.itusaca@gmail.com>
2024-04-28 13:45:26 -0500

    add resources

---
+++ app/src/main/java/com/upc/appcentroidiomas/AllProductsListFragment.java
@@ -0,0 +1,126 @@
+package com.upc.appcentroidiomas;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.upc.appcentroidiomas.api.ApiContants;
+import com.upc.appcentroidiomas.data.LoginDataSource;
+import com.upc.appcentroidiomas.data.LoginRepository;
+import com.upc.appcentroidiomas.data.model.LoggedInUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class AllProductsListFragment extends Fragment {
+    EditText txtFilter;
+    Button btnSearch;
+    RecyclerView recyclerProducts;
+    ArrayList<Product> products = new ArrayList<>();
+    AllProductsAdapter productsAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_list_products, container, false);
+        super.onCreate(savedInstanceState);
+
+        txtFilter = view.findViewById(R.id.txtFilter);
+        btnSearch = view.findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Search();
+            }
+        });
+
+        recyclerProducts = view.findViewById(R.id.recyclerProducts);
+
+        Search();
+
+        return view;
+    }
+
+    private void Search(){
+        String criteria = txtFilter.getText().toString();
+        LoggedInUser loggedInUser = LoginRepository.getInstance(new LoginDataSource(), this.getContext()).getLoggedUser();
+
+        String url = ApiContants.BASE_URL + "products/filtered-by-description?description="+criteria;
+
+        //if (!criteria.equals("")){
+        //  url = url + "" + criteria;
+        //}
+
+        StringRequest peticion = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onResponse(String response) {
+                try{
+                    products.clear();
+                    JSONArray jsonArray= new JSONArray(response);
+                    for (int i = 0; i < jsonArray.length(); i++){
+                        JSONObject object = jsonArray.getJSONObject(i);
+
+                        Product _product = new Product();
+                        _product.setId(object.getInt("id"));
+                        _product.setUserId(object.getInt("userId"));
+                        _product.setDescription(object.getString("description"));
+                        _product.setLocation(object.getString("location"));
+                        _product.setQuantity(object.getInt("quantity"));
+                        _product.setPrice(object.getDouble("price"));
+                        _product.setHarvestDate(object.getString("harvestDate"));
+                        _product.setCreatedAt(object.getString("createdAt"));
+                        _product.setCreatedBy(object.getString("createdBy"));
+                        _product.setProductTypeId(object.getInt("productTypeId"));
+                        _product.setProductTypeName(object.getString("productTypeName"));
+                        _product.setProductPresentationId(object.getInt("productPresentationId"));
+                        _product.setProductPresentationUnit(object.getString("productPresentationUnit"));
+                        _product.setDefaultImageUrl(object.getString("defaultImageUrl"));
+
+                        products.add(_product);
+                    }
+
+                    if (products.size() == 0 ){
+                        Toast.makeText(getContext(), "No se encontraron productos con su filtro.", Toast.LENGTH_LONG).show();
+                    }
+
+                    productsAdapter = new AllProductsAdapter(getContext(), products);
+                    recyclerProducts.setAdapter(productsAdapter);
+                    recyclerProducts.setLayoutManager(new LinearLayoutManager(getContext()));
+
+                }catch (JSONException e){
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        queue.add(peticion);
+    }
+}
--- app/src/main/java/com/upc/appcentroidiomas/utils/AndroidUtil.java
+++ app/src/main/java/com/upc/appcentroidiomas/utils/AndroidUtil.java
@@ -1,4 +1,4 @@
-package com.example.easychat.utils;
+package com.upc.appcentroidiomas.utils;
 
 import android.content.Context;
 import android.content.Intent;
@@ -7,34 +7,30 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
-import com.example.easychat.model.UserModel;
-import com.google.firebase.firestore.auth.User;
+import com.upc.appcentroidiomas.data.model.UserInformationModel;
 
 public class AndroidUtil {
-
-   public static  void showToast(Context context,String message){
-       Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
 
-    public static void passUserModelAsIntent(Intent intent, UserModel model){
-       intent.putExtra("username",model.getUsername());
-       intent.putExtra("phone",model.getPhone());
-       intent.putExtra("userId",model.getUserId());
-        intent.putExtra("fcmToken",model.getFcmToken());
-
+    public static UserInformationModel getUserModelFromIntent(Intent intent){
+        UserInformationModel userInformationModel = new UserInformationModel();
+        userInformationModel.id = intent.getIntExtra("userIdTo", 0);
+        userInformationModel.displayName = (intent.getStringExtra("displayNameTo"));
+        userInformationModel.imageUrl = (intent.getStringExtra("imageUrl"));
+        return userInformationModel;
     }
 
-    public static UserModel getUserModelFromIntent(Intent intent){
-        UserModel userModel = new UserModel();
-        userModel.setUsername(intent.getStringExtra("username"));
-        userModel.setPhone(intent.getStringExtra("phone"));
-        userModel.setUserId(intent.getStringExtra("userId"));
-        userModel.setFcmToken(intent.getStringExtra("fcmToken"));
-        return userModel;
-    }
+    public static void setProfilePic(Context context, Uri imageUri, ImageView imageView) {
+        Glide.with(context)
+                .load(imageUri)
+                .apply(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
+                .apply(new RequestOptions().skipMemoryCache(true))
+                .apply(RequestOptions.circleCropTransform())
+                .into(imageView);
 
-    public static void setProfilePic(Context context, Uri imageUri, ImageView imageView){
-        Glide.with(context).load(imageUri).apply(RequestOptions.circleCropTransform()).into(imageView);
     }
 }
